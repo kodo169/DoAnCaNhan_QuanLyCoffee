00adbec46625b3b254ece5620c48ea912405d8f2
Vũ Huỳnh <vuhuynh1692003@gmail.com>
2024-05-07 21:36:44 +0700

    ok

--- DoAnCaNhan_QuanLyCoffee/GUI/Login.cs
+++ DoAnCaNhan_QuanLyCoffee/GUI/Login.cs
@@ -49,6 +49,7 @@ namespace DoAnCaNhan_QuanLyCoffee
 
         public string RandomString(int length)
         {
+            //???
             Random random = new Random();
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
             return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
